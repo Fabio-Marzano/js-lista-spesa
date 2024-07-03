cc4a0050bda043bf2ed1aa7d0913b5afa6e28e63
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-03 18:22:45 +0200

    [UPD] create-list-and-ciclo-while

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const lista_spesa = ['Pomodoro', 'Patate', 'Pane', 'Pasta', 'Insalata'];
+//RECUPERO L'ELEMENTO CONTENENTE I VALORI DELL'ARRAY//
+const ul = document.getElementById('lista-spesa');
+//AVVIO CICLO ARRAY CON WHILE//
+let i = 0;
+//DEFINISCO CONDIZIONE FINE CICLO//
+while(i < lista_spesa.length) {
+    //CREO NUOVA LISTA//
+    const li = document.createElement('li');
+}
