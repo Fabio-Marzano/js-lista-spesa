1d94357a90742d7839a57cb065ba4c5db41fbd8a
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-04 00:03:42 +0200

    [UPD] create-list-spesa

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-const lista_spesa = ['Pomodoro', 'Patate', 'Pane', 'Pasta', 'Insalata'];
-//RECUPERO L'ELEMENTO CONTENENTE I VALORI DELL'ARRAY//
-const ul = document.getElementById('lista-spesa');
-//AVVIO CICLO ARRAY CON WHILE//
-let i = 0;
-//DEFINISCO CONDIZIONE FINE CICLO//
-while(i < lista_spesa.length) {
+const ingredienti = ['Pomodoro', 'Patate', 'Pane', 'Pasta', 'Insalata', 'Uova',];
+
+const lista_spesa = document.getElementById('lista-spesa');
+
+for (let i = 0; i < ingredienti.length; i++) {
+
     //CREO NUOVA LISTA//
     const li = document.createElement('li');
+    //INSERISCO IL TESTO //
+    li.innertext = ingredienti[i];
+
+    lista_spesa.append(li);
 }
+
+
+
